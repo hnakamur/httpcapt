def39994396d6674e76b14035bf17b2a2978f253
Hiroaki Nakamura <hnakamur@gmail.com>
2023-02-23 14:52:16 +0900

    Use gopacket/pcapgo for pure go packet capture

--- pcap_handle_pure_go.go
+++ pcap_handle_pure_go.go
@@ -6,23 +6,40 @@ import (
 	"time"
 
 	"github.com/google/gopacket/layers"
-	"github.com/packetcap/go-pcap"
+	"github.com/google/gopacket/pcapgo"
+	"github.com/packetcap/go-pcap/filter"
+	"golang.org/x/net/bpf"
 )
 
 var _ PcapHandle = (*pureGoPcapHandle)(nil)
 
 type pureGoPcapHandle struct {
-	*pcap.Handle
+	*pcapgo.EthernetHandle
 }
 
-func openLivePcapHandle(device string, snaplen int32, promiscuous bool, timeout time.Duration) (PcapHandle, error) {
-	h, err := pcap.OpenLive(device, snaplen, promiscuous, timeout, false)
+func openLivePcapHandle(device string, snaplen int32, promiscuous bool, _ time.Duration) (PcapHandle, error) {
+	h, err := pcapgo.NewEthernetHandle(device)
 	if err != nil {
 		return nil, err
 	}
-	return &pureGoPcapHandle{Handle: h}, nil
+	h.SetCaptureLength(int(snaplen))
+	h.SetPromiscuous(promiscuous)
+	return &pureGoPcapHandle{EthernetHandle: h}, nil
 }
 
 func (h *pureGoPcapHandle) LinkType() layers.LinkType {
-	return layers.LinkType(h.Handle.LinkType())
+	return layers.LinkTypeEthernet
+}
+
+func (h *pureGoPcapHandle) SetBPFFilter(expr string) error {
+	e := filter.NewExpression(expr)
+	insts, err := e.Compile().Compile()
+	if err != nil {
+		return err
+	}
+	rawInsts, err := bpf.Assemble(insts)
+	if err != nil {
+		return err
+	}
+	return h.EthernetHandle.SetBPF(rawInsts)
 }
